b3c4735088e070fd6177eae2819ca6096a71dba2
“TijesuRejoice” <tijesuadeleke94@gmail.com>
2023-07-10 10:03:21 -0700

    Task two

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,36 +1,29 @@
 #!/usr/bin/env python3
-""" execute multiple coroutines at the same time with async """
+""" Measure the runtime """
 
 import asyncio
 import random
-from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """ execute wait_random from 0-basic_async_syntax.py file
-    n times with the specified max_delay
-
+def measure_time(n: int, max_delay: int = 10) -> float:
+    """ measures the total execution time for wait_n(n, max_delay)
     Parameters
     ----------
     n : int
-      number of times to execute wait_random
+        number of execution
     max_delay : int
         maximum delay
 
     Returns
     -------
-    list
-        list of all the delays (float values) returned from wait_random
+    float
+        runtime per execution (total_time /n)
     """
-    spawn_list = []
-    delay_list = []
-    for i in range(n):
-        delayed_task = asyncio.create_task(wait_random(max_delay))
-        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
-        spawn_list.append(delayed_task)
+    elapsed_time: float
 
-    for spawn in spawn_list:
-        await spawn
-
-    return delay_list
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.perf_counter() - start_time
+    return total_time / n
